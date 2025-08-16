6c77d1b1195b9d5c72ed9fd98666b167b314272c
Satwikishankar <110528743+Satwikishankar@users.noreply.github.com>
2025-08-16 22:19:00 +0530

    33092

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
+
+def homepage(request):
+    return render(request, "homepage.html", {
+        "restaurant_name": getattr(settings, "RESTAURANT_NAME", "My Restaurant")
+    })

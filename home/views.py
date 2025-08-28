a473684f73a631419a80d8506861491e68210070
Satwikishankar <110528743+Satwikishankar@users.noreply.github.com>
2025-08-28 22:20:39 +0530

    39653

--- home/views.py
+++ home/views.py
@@ -15,3 +15,13 @@ def home(request):
     return render(request, "about.html", {
         "phone_number": settings.RESTAURANT_PHONE
     })
+
+
+class MenuAPIView(APIView):
+    def get(self, request):
+        menu = [
+            {"name": "Cheese Pizza", "description": "Classic pizza with cheese & tomato", "price": 250},
+            {"name": "Pasta", "description": "Creamy Alfreddo", "price": 300},
+            {"name": "Caesar Salad", "description": "Fresh romaine", "price": 200}
+        ]
+        return Response(menu)

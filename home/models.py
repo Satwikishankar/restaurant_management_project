1fc4439f1627341c54f964ca513c8b3310ddb080
Satwikishankar <110528743+Satwikishankar@users.noreply.github.com>
2025-08-28 22:43:11 +0530

    39652

--- home/models.py
+++ home/models.py
@@ -8,3 +8,22 @@ class Feedback(models.Model):
 
     def __str__(self):
         return self.comment[:50]
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+    
+class Order(models.Model):
+    customer_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    items = models.ManytoManyField(Menu)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id} by {self.customer_name}"

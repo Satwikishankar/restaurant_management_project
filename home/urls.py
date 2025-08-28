c37bebab1b7270ba283fc98c14ddb736597ea1e8
Satwikishankar <110528743+Satwikishankar@users.noreply.github.com>
2025-08-28 22:23:52 +0530

    39653

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,7 @@ from .views import *
 
 urlpatterns = [
     path("", views.homepage, name="home"),
+    path('about/', home, name="about"),
+    path('menu/', MenuAPIView.as_view(), name="menu"),
+    path('', include('home.urls')),
 ]

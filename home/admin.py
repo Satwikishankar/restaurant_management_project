b13587cec14c8f5e9171927ba9ae29a9d46ac68f
Satwikishankar <110528743+Satwikishankar@users.noreply.github.com>
2025-08-28 22:48:52 +0530

    39652

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Menu, Order
 
 # Register your models here.
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("name", "price")
+    search_fields = ("name",)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "customer_name", "created_at")
+    search_fields = ("customer_name",)
+    filter_horizontal = ("items",)

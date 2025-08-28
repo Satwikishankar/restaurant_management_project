05cdca99e893265ffdb9e8963bd58e97f3242f76
Satwikishankar <110528743+Satwikishankar@users.noreply.github.com>
2025-08-28 23:10:42 +0530

    39651

--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -20,10 +21,19 @@ class Menu(models.Model):
 
     
 class Order(models.Model):
-    customer_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=15)
-    items = models.ManytoManyField(Menu)
+    STATUS_CHOICES = [
+        ("PENDING","Pending"),
+        ("CONFIRMED", "Confirmed"),
+        ("DELIVERED", "Delivered"),
+        ("CANCELLED", "Cancelled"),
+    ]
+
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    items = models.ManyToManyField(Menu, related_name="orders")
+    total_amount = models.DecimalField(max_digits = 8, decimal_places=2)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="PENDING")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Order #{self.id} by {self.customer_name}"
+        return f"Order #{self.id} by {self.customer.username}"
+    

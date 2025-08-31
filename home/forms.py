a6c48a2e509edcc022f57c4ec3e18917a395ca5f
Satwikishankar <110528743+Satwikishankar@users.noreply.github.com>
2025-08-31 17:26:22 +0530

    40295

--- home/forms.py
+++ home/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import Feedback
+from .models import Contact
 
 class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Feedback
         fields = ['comment']
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['name', 'email']

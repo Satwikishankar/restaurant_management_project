5cc8b5b6f6d42b2277435ae584615894675d7d74
Satwikishankar <110528743+Satwikishankar@users.noreply.github.com>
2025-09-02 23:21:51 +0530

    41160

--- home/views.py
+++ home/views.py
@@ -44,3 +44,15 @@ class MenuAPIView(APIView):
             {"name": "Caesar Salad", "description": "Fresh romaine", "price": 200}
         ]
         return Response(menu)
+
+
+class contact_view(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("contact")
+
+        else:
+            form = ContactForm()
+        return render(request, "contact.html", {"form": form})

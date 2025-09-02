683b9e56bec01505cf4f2856aef5a493d227a6e4
Satwikishankar <110528743+Satwikishankar@users.noreply.github.com>
2025-09-02 23:15:26 +0530

    41161

--- home/models.py
+++ home/models.py
@@ -42,4 +42,14 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order #{self.id} by {self.customer.username}"
-    
+
+
+class RestaurantLocation(models.Model):
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=10)
+
+
+    def __str__(self):
+        return f"{self.address}, {self.city}, {self.state} {self.zip_code}"

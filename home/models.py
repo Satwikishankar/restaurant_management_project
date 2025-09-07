515ca3cf3883b30d955a6d5d787a9d7669a29b64
Satwikishankar <110528743+Satwikishankar@users.noreply.github.com>
2025-09-07 23:28:14 +0530

    42679

--- home/models.py
+++ home/models.py
@@ -13,6 +13,7 @@ class Feedback(models.Model):
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
+    message = models.TextField()
 
     def __str__(self):
         return self.name

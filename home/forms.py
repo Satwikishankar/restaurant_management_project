60cd0f17732bbfb33cbb5abd93d6d83aa0047893
Satwikishankar <110528743+Satwikishankar@users.noreply.github.com>
2025-09-23 21:05:04 +0530

    43342

--- home/forms.py
+++ home/forms.py
@@ -11,3 +11,15 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['name', 'email', 'message']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not email:
+            raise forms.ValidationError("Email is required.")
+        return email
+
+    def clean_message(self):
+        message = self.cleaned_data.get('message')
+        if not message or len(message.strip()) <10:
+            raise forms.ValidationError("Message must be atleast 10 characters long.")
+        return message
